3c9ddeedad0bce8a58c95b56334b1de09c924760
billran <kentra@pacbell.net>
2016-12-09 19:55:15 -0800

    Improved stress test with multiple threads

---
+++ src/test/java/org/point85/uom/test/StressTestRunner.java
@@ -0,0 +1,29 @@
+package org.point85.uom.test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class StressTestRunner {
+	// thread pool service
+	private ExecutorService executorService = Executors.newCachedThreadPool();
+
+	// run each test in a thread
+	public void runTests() throws Exception {
+		for (int i = 0; i < 10; i++) {
+			final TestPerformance test = new TestPerformance();
+			executorService.execute(() -> {
+				try {
+					test.runSingleTest();
+					System.out.println("Executed test pass");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			});
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		StressTestRunner runner = new StressTestRunner();
+		runner.runTests();
+	}
+}
--- src/test/java/org/point85/uom/test/TestPerformance.java
+++ src/test/java/org/point85/uom/test/TestPerformance.java
@@ -14,69 +14,65 @@ import org.point85.uom.Unit;
 import org.point85.uom.UnitOfMeasure;
 import org.point85.uom.UnitType;
 
-public class StressTest {
+public class TestPerformance {
+
+	// unit map
 	private Map<UnitType, List<UnitOfMeasure>> unitListMap = new HashMap<>();
-	
+
 	private void addUnit(UnitOfMeasure uom) {
 		List<UnitOfMeasure> unitList = unitListMap.get(uom.getUnitType());
-		
+
 		if (unitList == null) {
 			unitList = new ArrayList<>();
 			unitListMap.put(uom.getUnitType(), unitList);
 		}
 		unitList.add(uom);
 	}
-	
+
 	@Test
-	public void runTest() throws Exception {
+	public void runSingleTest() throws Exception {
 		MeasurementSystem sys = MeasurementSystem.getUnifiedSystem();
-		
+
 		// load all defined units
 		sys.clearCache();
-		
+
 		for (Unit u : Unit.values()) {
 			UnitOfMeasure uom = sys.getUOM(u);
 			this.addUnit(uom);
 		}
-		
+
 		// for each unit type, execute the quantity operations
-		for (Entry<UnitType, List<UnitOfMeasure>>  entry : unitListMap.entrySet()) {
-			
+		for (Entry<UnitType, List<UnitOfMeasure>> entry : unitListMap.entrySet()) {
 			// run the matrix
 			for (UnitOfMeasure rowUOM : entry.getValue()) {
-				
+
 				// row quantity
 				Quantity rowQty = new Quantity(BigDecimal.TEN, rowUOM);
-				
+
 				for (UnitOfMeasure colUOM : entry.getValue()) {
-					
+
 					// column qty
 					Quantity colQty = new Quantity(BigDecimal.TEN, colUOM);
-					
+
 					// arithmetic operations
 					rowQty.add(colQty);
 					rowQty.subtract(colQty);
-					
+
 					// offsets are not supported
-					if (rowUOM.getOffset().compareTo(BigDecimal.ZERO) == 0 && colUOM.getOffset().compareTo(BigDecimal.ZERO) == 0) {
+					if (rowUOM.getOffset().compareTo(BigDecimal.ZERO) == 0
+							&& colUOM.getOffset().compareTo(BigDecimal.ZERO) == 0) {
 						rowQty.multiply(colQty);
 						rowQty.divide(colQty);
 						rowQty.invert();
 					}
-		
+
 					rowQty.convert(colUOM);
 					rowQty.equals(colQty);
 					rowQty.getAmount();
 					rowQty.getUOM();
-					rowQty.toString();					
+					rowQty.toString();
 				}
 			}
 		}
 	}
-
-	public static void main(String[] args) throws Exception {
-		StressTest test = new StressTest();
-		test.runTest();
-	}
-
 }
