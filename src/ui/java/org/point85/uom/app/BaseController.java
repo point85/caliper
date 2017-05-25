7a7ba8542be177efc47d3c04ef00b0c937440022
billran <kentra@pacbell.net>
2017-05-25 10:54:30 -0700

    Initial submission of Unit Of Measure application

---
+++ src/ui/java/org/point85/uom/app/BaseController.java
@@ -0,0 +1,306 @@
+/*
+MIT License
+
+Copyright (c) 2017 Kent Randall
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package org.point85.uom.app;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.TreeMap;
+
+import org.point85.uom.Prefix;
+import org.point85.uom.UnitOfMeasure;
+import org.point85.uom.UnitType;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
+/**
+ * Base class for the UOM conversion and editor controllers
+ * 
+ * @author Kent Randall
+ *
+ */
+abstract class BaseController {
+	// max and min number of decimal places to show
+	private static final int MAX_DIGITS = 9;
+	private static final int MIN_DIGITS = 0;
+
+	// no text
+	protected static final String EMPTY_STRING = "";
+
+	// persistent measurement system
+	private PersistentMeasurementSystem sys = PersistentMeasurementSystem.getSystem();
+
+	// list of UnitTypes
+	private ObservableList<String> unitTypes = FXCollections.observableArrayList();
+
+	// list of Prefixes
+	private ObservableList<String> prefixes = FXCollections.observableArrayList();
+
+	// Reference to the main application
+	private CaliperApp app;
+
+	// the persistent measurement system
+	protected PersistentMeasurementSystem getPersistentSystem() {
+		return this.sys;
+	}
+
+	// Get display strings for UOMs of the specified type
+	protected ObservableList<String> getUnitsOfMeasure(String type) throws Exception {
+		ObservableList<String> displayStrings = FXCollections.observableArrayList();
+
+		// UnitType
+		UnitType unitType = UnitType.valueOf(type);
+
+		List<UnitOfMeasure> uoms = sys.getUnitsOfMeasure(unitType);
+
+		for (UnitOfMeasure uom : uoms) {
+			String displayString = toDisplayString(uom);
+			displayStrings.add(displayString);
+		}
+		Collections.sort(displayStrings);
+
+		return displayStrings;
+	}
+
+	// get the display strings for all UOM types
+	protected ObservableList<String> getUnitTypes() {
+		if (unitTypes.size() == 0) {
+			for (UnitType unitType : UnitType.values()) {
+				unitTypes.add(unitType.toString());
+			}
+			Collections.sort(unitTypes);
+		}
+		return unitTypes;
+	}
+
+	// get the display strings for all prefixes
+	protected ObservableList<String> getPrefixes() {
+		if (prefixes.size() == 0) {
+			for (Prefix prefix : Prefix.values()) {
+				prefixes.add(prefix.getPrefixName());
+			}
+			prefixes.add(EMPTY_STRING);
+			Collections.sort(prefixes);
+		}
+		return prefixes;
+	}
+
+	// get the display strings for custom symbols defined for the specified UOM
+	// type
+	protected ObservableList<String> getCustomSymbols(UnitType unitType) {
+
+		List<Object[]> rows = getPersistentSystem().fetchSymbolsAndNames(unitType);
+
+		List<String> displayStrings = new ArrayList<>(rows.size());
+
+		for (Object[] row : rows) {
+			String symbol = (String) row[0];
+			String name = (String) row[1];
+			displayStrings.add(toDisplayString(symbol, name));
+		}
+
+		return FXCollections.observableArrayList(displayStrings);
+	}
+
+	protected CaliperApp getApp() {
+		return this.app;
+	}
+
+	protected void setApp(CaliperApp app) {
+		this.app = app;
+	}
+
+	// display a general alert
+	protected ButtonType showAlert(Stage dialogStage, AlertType type, String title, String header, String errorMessage) {
+		// Show the error message.
+		Alert alert = new Alert(type); 
+		alert.initOwner(dialogStage);
+		alert.setTitle(title);
+		alert.setHeaderText(header);
+		alert.setContentText(errorMessage);
+
+		Optional<ButtonType> result = alert.showAndWait();
+		
+		ButtonType buttonType = null;
+		try {
+			buttonType = result.get();
+		} catch (NoSuchElementException e) {
+			
+		}
+		return buttonType;
+	}
+
+	// display an error dialog
+	protected void showErrorDialog(Stage dialogStage, String message) {
+		showAlert(dialogStage, AlertType.ERROR, "Application Error", "Exception", message);
+	}
+	
+	// display an error dialog
+	protected void showErrorDialog(Stage dialogStage, Exception e) {
+		String message = e.getMessage();
+		
+		if (message == null) {
+			message = e.getClass().getSimpleName();
+		}
+		showAlert(dialogStage, AlertType.ERROR, "Application Error", "Exception", message);
+	}
+	
+	// display an ok/cancel dialog
+	protected ButtonType showConfirmationDialog(Stage dialogStage, String message) {
+		return showAlert(dialogStage, AlertType.CONFIRMATION, "Confirmation", "Confirm Action", message);
+	}
+
+	// get the UOM from cache first, then from the database if not found
+	protected UnitOfMeasure getUOMForConversion(Prefix prefix, String symbol) throws Exception {
+		UnitOfMeasure uom = null;
+
+		if (symbol == null || symbol.length() == 0) {
+			return uom;
+		}
+
+		// look in cache first
+		uom = getPersistentSystem().getUOM(symbol);
+
+		if (uom == null) {
+			// database next
+			uom = getPersistentSystem().fetchUOMBySymbol(symbol, false);
+
+			if (uom != null) {
+				// cache it
+				getPersistentSystem().registerUnit(uom);
+			}
+		}
+
+		if (uom != null && prefix != null) {
+			uom = getPersistentSystem().getUOM(prefix, uom);
+		}
+
+		return uom;
+	}
+
+	// get the UOM from the database first, then from cache if not found
+	protected UnitOfMeasure getUOMForEditing(String symbol) throws Exception {
+		UnitOfMeasure uom = null;
+
+		if (symbol == null || symbol.length() == 0) {
+			return uom;
+		}
+
+		// look in database first
+		uom = getPersistentSystem().fetchUOMBySymbol(symbol, true);
+
+		if (uom != null) {
+			// cache it
+			getPersistentSystem().registerUnit(uom);
+		}
+
+		if (uom == null) {
+			// get from cache next
+			uom = getPersistentSystem().getUOM(symbol);
+		}
+
+		// bring referenced units into persistence context
+		getPersistentSystem().fetchReferencedUnits(uom);
+
+		return uom;
+	}
+
+	// create a string to represent the UOM
+	static String toDisplayString(UnitOfMeasure uom) {
+		StringBuffer sb = new StringBuffer();
+		sb.append(uom.getSymbol()).append(" (").append(uom.getName()).append(')');
+		return sb.toString();
+	}
+
+	// create a String from the UOM symbol and name
+	protected String toDisplayString(String symbol, String name) {
+		StringBuffer sb = new StringBuffer();
+		sb.append(symbol).append(" (").append(name).append(')');
+		return sb.toString();
+	}
+
+	// parse the UOM symbol out of the display string
+	protected String parseSymbol(String displayString) {
+		String symbol = null;
+
+		if (displayString != null) {
+			int idx = displayString.indexOf('(');
+			symbol = displayString.substring(0, idx - 1);
+		}
+		return symbol;
+	}
+
+	// removed formatting from decimal string
+	protected String removeThousandsSeparator(String formattedString) {
+		DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols();
+
+		StringBuffer sb = new StringBuffer();
+		sb.append(decimalFormatSymbols.getGroupingSeparator());
+		String separator = sb.toString();
+
+		String[] thousands = formattedString.split(separator);
+
+		sb = new StringBuffer();
+
+		for (String thousand : thousands) {
+			sb.append(thousand);
+		}
+		return sb.toString();
+	}
+
+	// format a BigDecimal nicely
+	protected String formatBigDecimal(BigDecimal decimal) {
+		NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.getDefault());
+		numberFormat.setGroupingUsed(true);
+		numberFormat.setMaximumFractionDigits(MAX_DIGITS);
+		numberFormat.setMinimumFractionDigits(MIN_DIGITS);
+		return numberFormat.format(decimal);
+	}
+
+	// print the symbol cache
+	protected void snapshotSymbolCache() {
+		Map<String, UnitOfMeasure> treeMap = new TreeMap<>(sys.getSymbolCache());
+
+		System.out.println("Symbol cache ...");
+		int count = 0;
+		for (Entry<String, UnitOfMeasure> entry : treeMap.entrySet()) {
+			count++;
+			System.out.println("(" + count + ") " + entry.getKey() + (", ") + entry.getValue());
+		}
+	}
+}
---
+++ src/ui/java/org/point85/uom/app/PersistentMeasurementSystem.java
@@ -0,0 +1,383 @@
+/*
+MIT License
+
+Copyright (c) 2017 Kent Randall
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package org.point85.uom.app;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import org.point85.uom.MeasurementSystem;
+import org.point85.uom.Unit;
+import org.point85.uom.UnitOfMeasure;
+import org.point85.uom.UnitOfMeasure.MeasurementType;
+import org.point85.uom.UnitType;
+
+// class to manage saving UOMs to a database using JPA
+class PersistentMeasurementSystem extends MeasurementSystem {
+	// JPA persistence unit name
+	private static final String PERSISTENCE_UNIT = "UOM";
+
+	// named queries
+	private static final String NQ_UOM_CATEGORIES = "UOM.Categories";
+	private static final String NQ_UOM_SYMBOLS = "UOM.Symbols";
+	private static final String NQ_UOM_SYMBOLS_IN_CATEGORY = "UOM.SymbolsInCategory";
+	private static final String NQ_UOM_BY_SYMBOL = "UOM.BySymbol";
+	private static final String NQ_UOM_BY_UNIT = "UOM.ByUnit";
+	private static final String NQ_UOM_BY_KEY = "UOM.ByKey";
+	private static final String NQ_UOM_CROSS_REF = "UOM.CrossRef";
+
+	// standard unified system
+	private static PersistentMeasurementSystem persistentSystem = new PersistentMeasurementSystem();
+
+	// entity manager factory
+	private EntityManagerFactory emf;
+
+	// entity manager
+	private EntityManager em;
+
+	private PersistentMeasurementSystem() {
+		super();
+	}
+
+	// get the EntityManager, create if necessary
+	private EntityManager getEntityManager() {
+		if (em == null) {
+			emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+			em = emf.createEntityManager();
+		}
+		return em;
+	}
+
+	/**
+	 * Singleton
+	 * 
+	 * @return PersistentMeasurementSystem
+	 */
+	public static PersistentMeasurementSystem getSystem() {
+		return persistentSystem;
+	}
+
+	// query for UOM based on its unique symbol
+	UnitOfMeasure fetchUOMBySymbol(String symbol, boolean cascade) throws Exception {
+		Map<String, Object> parameters = new HashMap<>();
+		parameters.put("symbol", symbol);
+
+		// query based on symbol
+		UnitOfMeasure uom = fetchUnitOfMeasure(PersistentMeasurementSystem.NQ_UOM_BY_SYMBOL, parameters);
+
+		if (uom != null && cascade) {
+			// bring referenced UOMs into the persistence context
+			fetchReferencedUnits(uom);
+		}
+
+		return uom;
+	}
+
+	// fetch recursively all referenced units to make them managed in the
+	// persistence unit
+	void fetchReferencedUnits(UnitOfMeasure uom) throws Exception {
+		String id = null;
+		UnitOfMeasure referenced = null;
+		UnitOfMeasure fetched = null;
+
+		// abscissa unit
+		referenced = uom.getAbscissaUnit();
+		if (referenced != null && !uom.isTerminal()) {
+			id = referenced.getSymbol();
+			fetched = fetchUOMBySymbol(id, true);
+
+			if (fetched != null) {
+				// already in database
+				uom.setAbscissaUnit(fetched);
+			}
+
+			// units referenced by the abscissa
+			fetchReferencedUnits(referenced);
+		}
+
+		// bridge abscissa unit
+		referenced = uom.getBridgeAbscissaUnit();
+		if (referenced != null) {
+			id = referenced.getSymbol();
+			fetched = fetchUOMBySymbol(id, true);
+
+			if (fetched != null) {
+				// already in database
+				uom.setBridgeConversion(uom.getBridgeScalingFactor(), fetched, uom.getBridgeOffset());
+			}
+		}
+
+		// UOM1 and UOM2
+		if (uom.getMeasurementType().equals(MeasurementType.PRODUCT)) {
+			// multiplier
+			referenced = uom.getMultiplier();
+			id = referenced.getSymbol();
+			fetched = fetchUOMBySymbol(id, true);
+
+			// multiplicand
+			UnitOfMeasure referenced2 = uom.getMultiplicand();
+			id = referenced2.getSymbol();
+			UnitOfMeasure fetched2 = fetchUOMBySymbol(id, true);
+
+			if (fetched != null && fetched2 != null) {
+				// already in database
+				uom.setProductUnits(fetched, fetched2);
+			}
+
+			// units referenced by UOM1 & 2
+			fetchReferencedUnits(referenced);
+			fetchReferencedUnits(referenced2);
+
+		} else if (uom.getMeasurementType().equals(MeasurementType.QUOTIENT)) {
+			// dividend
+			referenced = uom.getDividend();
+			id = referenced.getSymbol();
+			fetched = fetchUOMBySymbol(id, true);
+
+			// divisor
+			UnitOfMeasure referenced2 = uom.getDivisor();
+			id = referenced2.getSymbol();
+			UnitOfMeasure fetched2 = fetchUOMBySymbol(id, true);
+
+			if (fetched != null && fetched2 != null) {
+				// already in database
+				uom.setQuotientUnits(fetched, fetched2);
+			}
+
+			// units referenced by UOM1 & 2
+			fetchReferencedUnits(referenced);
+			fetchReferencedUnits(referenced2);
+
+		} else if (uom.getMeasurementType().equals(MeasurementType.POWER)) {
+			referenced = uom.getPowerBase();
+			id = referenced.getSymbol();
+			fetched = fetchUOMBySymbol(id, true);
+
+			if (fetched != null) {
+				// already in database
+				uom.setPowerUnit(fetched, uom.getPowerExponent());
+			}
+
+			// units referenced by the power base
+			fetchReferencedUnits(referenced);
+		}
+	}
+
+	// fetch UOM by its enumeration
+	UnitOfMeasure fetchUOMByUnit(Unit unit) throws Exception {
+		UnitOfMeasure uom = null;
+
+		if (unit == null) {
+			return uom;
+		}
+
+		// check in the database
+		Map<String, Object> parameters = new HashMap<>();
+		parameters.put("unit", unit);
+
+		// fetch by Unit enum
+		Object result = fetchUnitOfMeasure(NQ_UOM_BY_UNIT, parameters);
+
+		if (result != null) {
+			uom = (UnitOfMeasure) result;
+			fetchReferencedUnits(uom);
+
+		} else {
+			// not in db, get from pre-defined units
+			uom = getUOM(unit);
+
+			// fetch units that it references
+			fetchReferencedUnits(uom);
+		}
+
+		return uom;
+	}
+
+	// fetch UOM by its primary key
+	UnitOfMeasure fetchUOMByKey(Integer key) throws Exception {
+
+		Map<String, Object> parameters = new HashMap<>();
+		parameters.put("key", key);
+
+		Object result = fetchUnitOfMeasure(NQ_UOM_BY_KEY, parameters);
+
+		return (UnitOfMeasure) result;
+	}
+
+	// execute the named query
+	List<?> executeNamedQuery(String queryName, Map<String, Object> parameters) {
+		Query query = getEntityManager().createNamedQuery(queryName);
+
+		if (parameters != null) {
+			for (Entry<String, Object> entry : parameters.entrySet()) {
+				query.setParameter(entry.getKey(), entry.getValue());
+			}
+		}
+		return query.getResultList();
+	}
+
+	// fetch UOM by a named query
+	UnitOfMeasure fetchUnitOfMeasure(String queryName, Map<String, Object> parameters) throws Exception {
+		UnitOfMeasure uom = null;
+
+		Query query = getEntityManager().createNamedQuery(queryName);
+
+		if (parameters != null) {
+			for (Entry<String, Object> entry : parameters.entrySet()) {
+				query.setParameter(entry.getKey(), entry.getValue());
+			}
+		}
+		try {
+			uom = (UnitOfMeasure) query.getSingleResult();
+		} catch (Exception e) {
+			//
+		}
+		return uom;
+	}
+
+	// remove the UOM from the persistence context
+	void evictUOM(UnitOfMeasure entity) {
+		getEntityManager().detach(entity);
+	}
+
+	// save the UOM to the database
+	void saveUOM(UnitOfMeasure entity) throws Exception {
+		try {
+			// start transaction
+			getEntityManager().getTransaction().begin();
+
+			// merge this entity into the PU
+			getEntityManager().merge(entity);
+
+			// commit transaction
+			getEntityManager().getTransaction().commit();
+		} catch (Throwable t) {
+			// roll back transaction
+			if (getEntityManager().getTransaction().isActive()) {
+				getEntityManager().getTransaction().rollback();
+			}
+			throw new Exception(t.getMessage());
+		}
+	}
+
+	// delete the UOM from the database
+	void deleteUOM(UnitOfMeasure entity) throws Exception {
+		if (entity == null) {
+			return;
+		}
+
+		// check for cross references
+		List<UnitOfMeasure> uoms = getCrossReferences(entity);
+
+		if (uoms.size() != 0) {
+			List<String> displayStrings = new ArrayList<>(uoms.size());
+
+			for (UnitOfMeasure uom : uoms) {
+				displayStrings.add(BaseController.toDisplayString(uom));
+			}
+
+			throw new Exception("Unit of measure " + entity.getName()
+					+ " cannot be deleted.  It is being referenced by UOM(s) " + displayStrings + ".");
+		}
+
+		try {
+			// start transaction
+			getEntityManager().getTransaction().begin();
+
+			// delete
+			getEntityManager().remove(getEntityManager().merge(entity));
+
+			// commit transaction
+			getEntityManager().getTransaction().commit();
+		} catch (Throwable t) {
+			// roll back transaction
+			if (getEntityManager().getTransaction().isActive()) {
+				getEntityManager().getTransaction().rollback();
+			}
+			throw new Exception(t.getMessage());
+		}
+	}
+
+	// get any foreign UOM references
+	List<UnitOfMeasure> getCrossReferences(UnitOfMeasure uom) throws Exception {
+		Integer key = uom.getKey();
+
+		Query query = getEntityManager().createNamedQuery(NQ_UOM_CROSS_REF);
+		query.setParameter(1, key);
+
+		@SuppressWarnings("unchecked")
+		List<Integer> keys = (List<Integer>) query.getResultList();
+
+		List<UnitOfMeasure> referencedUOMs = new ArrayList<>(keys.size());
+
+		// get the referenced UOMs
+		for (Integer primaryKey : keys) {
+			UnitOfMeasure referenced = fetchUOMByKey(primaryKey);
+			referencedUOMs.add(referenced);
+		}
+
+		return referencedUOMs;
+	}
+
+	// fetch all defined categories
+	List<String> fetchCategories() {
+		@SuppressWarnings("unchecked")
+		List<String> categories = (List<String>) executeNamedQuery(PersistentMeasurementSystem.NQ_UOM_CATEGORIES, null);
+		Collections.sort(categories);
+		return categories;
+	}
+
+	// fetch symbols and their names for this UOM type
+	List<Object[]> fetchSymbolsAndNames(UnitType unitType) {
+		Map<String, Object> parameters = new HashMap<>();
+		parameters.put("type", unitType);
+
+		@SuppressWarnings("unchecked")
+		List<Object[]> values = (List<Object[]>) executeNamedQuery(PersistentMeasurementSystem.NQ_UOM_SYMBOLS,
+				parameters);
+
+		return values;
+	}
+
+	// get symbols and names in this category
+	List<Object[]> getSymbolsAndNames(String category) {
+		Map<String, Object> parameters = new HashMap<>();
+		parameters.put("category", category);
+
+		@SuppressWarnings("unchecked")
+		List<Object[]> values = (List<Object[]>) executeNamedQuery(
+				PersistentMeasurementSystem.NQ_UOM_SYMBOLS_IN_CATEGORY, parameters);
+
+		return values;
+	}
+}
